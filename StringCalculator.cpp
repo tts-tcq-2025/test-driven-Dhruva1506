bdd24dc3e138784f857fea69b3af0370b0a8e2b1
Dhruva1506 <vidushikumari15@gmail.com>
2025-09-29 18:22:01 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,59 +1,59 @@
 #include "StringCalculator.h"
-#include <sstream>
-#include <stdexcept>
+#include <vector>
+#include <string>
 #include <regex>
-#include <iostream>
-
-std::vector<std::string> StringCalculator::split(const std::string &s, const std::string &delimiter) {
-    std::vector<std::string> tokens;
-    std::regex re(delimiter);
-    std::sregex_token_iterator first{s.begin(), s.end(), re, -1}, last;
-    for (auto it = first; it != last; ++it) {
-        if (!it->str().empty())
-            tokens.push_back(it->str());
-    }
-    return tokens;
-}
+#include <stdexcept>
+#include <algorithm>
 
 int StringCalculator::add(const std::string &numbers) {
-    if (numbers.empty())
-        return 0;
+  if (numbers.empty()) return 0;
 
-    std::string nums = numbers;
-    std::string delimiter = ",|\n";
+  std::string nums = numbers;
+  std::string delimiter = ",|\n";
 
-    // Handle custom delimiter
-    if (nums.substr(0, 2) == "//") {
-        std::smatch match;
-        std::regex re("//(\\[.*\\]|.)\\n(.*)", std::regex::dotall);
-        if (std::regex_match(nums, match, re)) {
-            std::string delimDef = match[1].str();
-            nums = match[2].str();
-            // Remove brackets for multi-char delimiter
-            if (delimDef.front() == '[' && delimDef.back() == ']')
-                delimDef = delimDef.substr(1, delimDef.size() - 2);
-            delimiter = std::regex_replace(delimDef, std::regex(R"([.^$|()\\+*?{}[\]])"), R"(\\$&)");
-        }
+  // Handle custom delimiter
+  if (nums.rfind("//", 0) == 0) {
+    std::smatch match;
+    std::regex re("//(\\[.*\\]|.)\\n(.*)");
+    if (std::regex_match(nums, match, re)) {
+      std::string delimDef = match[1].str();
+      nums = match[2].str();
+      if (delimDef.front() == '[' && delimDef.back() == ']')
+        delimDef = delimDef.substr(1, delimDef.size() - 2);
+
+      // Escape regex special characters
+      delimiter = std::regex_replace(delimDef,
+                                     std::regex(R"([.^$|()\\+*?{}[\]])"),
+                                     R"(\\$&)");
     }
+  }
 
-    auto tokens = split(nums, delimiter);
-    int result = 0;
-    std::vector<int> negatives;
+  std::regex re_delim(delimiter);
+  std::sregex_token_iterator it(nums.begin(), nums.end(), re_delim, -1);
+  std::sregex_token_iterator end;
 
-    for (const auto &token : tokens) {
-        int num = std::stoi(token);
-        if (num < 0)
-            negatives.push_back(num);
-        else if (num <= 1000)
-            result += num;
+  int result = 0;
+  std::vector<int> negatives;
+
+  // Iterate tokens using lambda
+  std::for_each(it, end, [&](const std::string &token) {
+    if (token.empty()) return;
+    int num = std::stoi(token);
+    if (num < 0) {
+      negatives.push_back(num);
+    } else if (num <= 1000) {
+      result += num;
     }
+  });
 
-    if (!negatives.empty()) {
-        std::string msg = "negatives not allowed: ";
-        for (auto n : negatives)
-            msg += std::to_string(n) + " ";
-        throw std::runtime_error(msg);
+  if (!negatives.empty()) {
+    std::string msg = "negatives not allowed: ";
+    for (size_t i = 0; i < negatives.size(); ++i) {
+      msg += std::to_string(negatives[i]);
+      if (i != negatives.size() - 1) msg += " ";
     }
+    throw std::runtime_error(msg);
+  }
 
-    return result;
+  return result;
 }
